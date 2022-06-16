0074ce2cbb09bc0ac95ee0657da5db59bc3837a4
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-06-16 17:49:31 +0100

    added a restriction to images only for file upload

--- src/components/pages/Stories.jsx
+++ src/components/pages/Stories.jsx
@@ -152,7 +152,6 @@ function Stories() {
                     className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     aria-describedby="user_avatar_help"
                     id="user_avatar"
-                    accept="jpeg, png, jpg"
                     type="file"
                     required
                     accept="image/*,capture=camera"
